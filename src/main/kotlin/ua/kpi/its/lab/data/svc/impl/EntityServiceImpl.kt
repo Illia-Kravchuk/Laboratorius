509fe870eea641f83ec385ebda587cc1b7c97881
Illia-Kravchuk <107136872+Illia-Kravchuk@users.noreply.github.com>
2024-05-20 20:30:30 +0300

    Update EntityServiceImpl.kt

--- src/main/kotlin/ua/kpi/its/lab/data/svc/impl/EntityServiceImpl.kt
+++ src/main/kotlin/ua/kpi/its/lab/data/svc/impl/EntityServiceImpl.kt
@@ -1,12 +1,25 @@
 package ua.kpi.its.lab.data.svc.impl
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import ua.kpi.its.lab.data.svc.EntityService
+import ua.kpi.its.lab.data.entity.Educational
+import ua.kpi.its.lab.data.entity.Discipline
+import ua.kpi.its.lab.data.repo.EducationalRepository
+import ua.kpi.its.lab.data.repo.DisciplineRepository
+import ua.kpi.its.lab.data.svc.EducationalService
+import ua.kpi.its.lab.data.svc.DisciplineService
 
 @Service
-class EntityServiceImpl @Autowired constructor(
-    // Your code here
-): EntityService {
-    // Your code here
+class MagazineServiceImpl(private val repo: EducationalRepository) : EducationalService {
+    override fun create(educational: Educational) = repo.save(educational)
+    override fun retrieve(id: Long) = repo.findById(id).orElse(null)
+    override fun update(educational: Educational) = repo.save(educational)
+    override fun delete(id: Long) = repo.deleteById(id)
+}
+
+@Service
+class ScientificArticleServiceImpl(private val repo: DisciplineRepository) : DisciplineService {
+    override fun create(discipline: Discipline) = repo.save(discipline)
+    override fun retrieve(id: Long) = repo.findById(id).orElse(null)
+    override fun update(discipline: Discipline) = repo.save(discipline)
+    override fun delete(id: Long) = repo.deleteById(id)
 }
