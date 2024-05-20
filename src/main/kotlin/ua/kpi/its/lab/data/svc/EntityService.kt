4803457b14c82241cefb139793cc0a045b429481
Illia-Kravchuk <107136872+Illia-Kravchuk@users.noreply.github.com>
2024-05-20 20:32:41 +0300

    Update EntityService.kt

--- src/main/kotlin/ua/kpi/its/lab/data/svc/EntityService.kt
+++ src/main/kotlin/ua/kpi/its/lab/data/svc/EntityService.kt
@@ -1,5 +1,18 @@
 package ua.kpi.its.lab.data.svc
 
-interface EntityService {
-    // Your code here
-}
+import ua.kpi.its.lab.data.entity.Educational
+import ua.kpi.its.lab.data.entity.Discipline
+
+interface EducationalService {
+     fun create(educational: Educational): Educational
+     fun retrieve(id: Long): Educational?
+     fun update(educational: Educational): Educational
+     fun delete(id: Long)
+ }
+
+ interface DisciplineService {
+     fun create(discipline: Discipline): Discipline
+     fun retrieve(id: Long): Discipline?
+     fun update(discipline: Discipline): Discipline
+     fun delete(id: Long)
+ }
