c952cab57f52783303acd0eeeb03fe54a4ae4f67
Illia-Kravchuk <107136872+Illia-Kravchuk@users.noreply.github.com>
2024-05-20 19:00:28 +0300

    Update Entity.kt

--- src/main/kotlin/ua/kpi/its/lab/data/entity/Entity.kt
+++ src/main/kotlin/ua/kpi/its/lab/data/entity/Entity.kt
@@ -1,8 +1,42 @@
 package ua.kpi.its.lab.data.entity
 
-class ExampleEntity : Comparable<ExampleEntity> {
-    override fun compareTo(other: ExampleEntity): Int {
-        TODO("Your code in your entity")
-    }
-}
-// Your code here
+import jakarta.persistence.*
+import java.util.*
+
+
+@Entity
+data class Magazine(
+     @Id @GeneratedValue(strategy = GenerationType.AUTO)
+     val id: Long = 0,
+
+     val name: String,
+     val topic: String,
+     val language: String,
+     val establishDate: Date,
+     val issn: String,
+     val priceRecommended: String,
+     val periodicity: Boolean,
+
+ ) : Comparable<Magazine> {
+     override fun compareTo(other: Magazine): Int {
+         val nameComparison = name.compareTo(other.name)
+         return if (nameComparison != 0) nameComparison else id.compareTo(other.id)
+     }
+ }
+
+ @Entity
+ data class ScientificArticle(
+     @Id @GeneratedValue(strategy = GenerationType.AUTO)
+     val id: Long = 0,
+
+     val name: String,
+     val author: String,
+     val writingDate: Date,
+     val wordCount: String,
+     val numberLinks: Long,
+     val origLang: Boolean,
+
+     @ManyToOne
+     @JoinColumn(name = "magazine_id")
+     val magazine: Magazine?
+ )
